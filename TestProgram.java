80899e028f0ac447e7c3debb2ced3b66c6755e41
dilsadmohammed4 <dilsadmohammed4@gmail.com>
2023-10-08 18:56:24 +0530

    1st commit

---
+++ TestProgram.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+/**
+ * TestProgram
+ */
+public class TestProgram {
+
+    public static void main(String[] args) {
+        System.out.println("This is Demo Program");
+        System.out.println("Output from java Program");
+        System.out.println(new Date());
+    }
+}
